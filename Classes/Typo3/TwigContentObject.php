50c69c5af7744ed6148a1c8441f99cc4f9e50814
René Nitzsche <rene@system25.de>
2024-01-05 10:05:16 +0100

    Add di-config

---
+++ Classes/Typo3/TwigContentObject.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace System25\T3twigs\Typo3;
+
+/***************************************************************
+ * Copyright notice
+ *
+ * (c) 2024 Rene Nitzsche (rene@system25.de)
+ * All rights reserved
+ *
+ * This script is part of the TYPO3 project. The TYPO3 project is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use DMK\T3twig\Twig\RendererTwig as Renderer;
+use DMK\T3twig\Twig\T3TwigException;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Frontend\ContentObject\AbstractContentObject;
+use tx_rnbase;
+
+class TwigContentObject extends AbstractContentObject
+{
+    public function __construct($cObject = null)
+    {
+        if ($cObject) {
+            parent::__construct($cObject);
+        }
+    }
+
+    /**
+     * @param string                $name
+     * @param array                 $configuration
+     * @param string                $typoscriptKey
+     * @param ContentObjectRenderer $contentObject
+     *
+     * @return string
+     */
+    public function cObjGetSingleExt($name, array $configuration, $typoscriptKey, $contentObject)
+    {
+        $this->cObj = $contentObject;
+
+        return $this->render($configuration);
+    }
+
+    /**
+     * Rendering the cObject, TEMPLATE.
+     *
+     * @param array $conf Array of TypoScript properties
+     *
+     * @return string Output
+     *
+     * @see substituteMarkerArrayCached()
+     *
+     * @throws T3TwigException
+     */
+    public function render(
+        $conf = []
+    ) {
+        $content = '';
+
+        $configurations = $this->buildConfigurations($conf);
+        $renderer = Renderer::instance(
+            $configurations,
+            ''
+        );
+        $contextData = $this->getContext($configurations);
+
+        $content .= $renderer->render($contextData);
+
+        return $content;
+    }
+
+    /**
+     * Builds the configuration object based on the conf.
+     *
+     * @param array $conf
+     *
+     * @return \Sys25\RnBase\Configuration\Processor
+     */
+    private function buildConfigurations(
+        array $conf
+    ) {
+        /** @var $configurations \Sys25\RnBase\Configuration\ConfigurationInterface */
+        $configurations = tx_rnbase::makeInstance(
+            \Sys25\RnBase\Configuration\Processor::class
+        );
+        $configurations->init(
+            $conf,
+            $this->getContentObject(),
+            't3twig',
+            't3twig'
+        );
+
+        return $configurations;
+    }
+
+    /**
+     * Compile rendered content objects in variables array ready to assign to the view.
+     *
+     * @param \Sys25\RnBase\Configuration\Processor $configurations
+     *
+     * @return array the variables to be assigned
+     *
+     * @throws T3TwigException
+     */
+    protected function getContext($configurations)
+    {
+        $contextData = [];
+        $contextNames = $configurations->getKeyNames('context.');
+        if (empty($contextNames)) {
+            $contextNames = $configurations->getKeyNames('variables.');
+        }
+        $reservedVariables = ['data', 'current'];
+        foreach ($contextNames as $key) {
+            if (!in_array($key, $reservedVariables)) {
+                $contextData[$key] = $configurations->get('context.'.$key, true);
+            } else {
+                throw new T3TwigException('Cannot use reserved name "'.$key.'" as variable name in TWIGTEMPLATE.', 1288095720);
+            }
+        }
+
+        $contextData['data'] = $this->getContentObject()->data;
+        $contextData['current'] = $this->getContentObject()->data[$this->getContentObject()->currentValKey];
+
+        return $contextData;
+    }
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -4,10 +4,12 @@ if (!(defined('TYPO3') || defined('TYPO3_MODE'))) {
     exit('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['FE']['ContentObjects'] = array_merge(
-    $GLOBALS['TYPO3_CONF_VARS']['FE']['ContentObjects'],
-    [
-        'TWIGTEMPLATE' => \System25\T3twigs\ContentObject\TwigContentObject::class,
-    ]
-);
+if (!\Sys25\RnBase\Utility\TYPO3::isTYPO124OrHigher()) {
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['ContentObjects'] = array_merge(
+        $GLOBALS['TYPO3_CONF_VARS']['FE']['ContentObjects'] ?? [],
+        [
+            'TWIGTEMPLATE' => \System25\T3twigs\ContentObject\TwigContentObject::class,
+        ]
+    );
+}
 
